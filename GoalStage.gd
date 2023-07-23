6c0b9fe6dff79ae224fdc626d7110f94065d70a7
astnohk <kon.bass@gmail.com>
2023-07-23 14:29:53 +0900

    Bug fix and Improve World Environment
    * Add volumetric fog.
    * Fixed goal detection algorithm bugs.

--- GoalStage.gd
+++ GoalStage.gd
@@ -33,10 +33,14 @@ func _physics_process(delta):
 func _on_area_3d_body_entered(body):
 	if body.name in Colliding.keys():
 		Colliding[body.name] = true
-	Goal = true
-	for key in Colliding.keys():
-		Goal = Goal and Colliding[key]
+	updateGoal()
 
 func _on_area_3d_body_exited(body):
 	if body.name in Colliding.keys():
 		Colliding[body.name] = false
+	updateGoal()
+
+func updateGoal():
+	Goal = true
+	for key in Colliding.keys():
+		Goal = Goal and Colliding[key]
