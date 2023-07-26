686e2b1a969e80dd4aa41fdbff733400f1a9874f
astnohk <kon.bass@gmail.com>
2023-07-27 01:58:10 +0900

    Improve behaviors of RocketBase

--- Objects/RocketBase/RocketBase.gd
+++ Objects/RocketBase/RocketBase.gd
@@ -1,11 +1,20 @@
 extends Node3D
 
+var animation_player: AnimationPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var animation_player: AnimationPlayer = get_node("AnimationPlayer")
+	animation_player = get_node("AnimationPlayer")
+	animation_player.connect("animation_finished", _animation_finished)
+	# Start "Open" animation
 	animation_player.current_animation = "Open"
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func _animation_finished(anim_name):
+	if anim_name == "Open":
+		animation_player.current_animation = "Opening"
+	elif anim_name == "Close":
+		animation_player.current_animation = "Closing"
